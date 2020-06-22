e3d1ac6fd093eb63e0ae5b648c5dcb62b25acc18
Abdul Raffay <abdul-raffay@nytrotech.com>
2020-06-22 16:12:06 +0500

    Teacher Extracting from Courses:

--- themes/Edu/index.php
+++ themes/Edu/index.php
@@ -3,7 +3,7 @@
     <?php
 
         $teacher = new WP_Query(array(
-            'post_type' => 'teacher'
+            'post_type' => 'teacher',
         ));
 
         while($teacher->have_posts()){
@@ -11,8 +11,37 @@
             ?>
 
             <a href="<?php the_permalink(); ?>"><h2><small><?php the_field('academic_titles'); ?></small> <?php the_title(); ?></h2></a>
-            <p>Teaches: <?php the_field('subject'); ?></p>
             <img src="<?php the_post_thumbnail_url('thumbnail'); ?>" alt="">
+            <hr>
+
+            <?php 
+        }
+        wp_reset_postdata();
+
+        ?>
+
+        <hr><hr>
+
+        <?php
+
+        $course = new WP_Query(array(
+            'post_type' => 'course'
+        ));
+
+        while($course->have_posts()){
+            $course->the_post();
+            ?>
+
+            <a href="<?php the_permalink(); ?>"><h2>Course Name: <?php the_title(); ?></h2></a>
+            <?php  
+            
+            $teacherCourse = get_field('teacher_name');
+            foreach($teacherCourse as $teacher){
+                echo $teacher->post_title;
+            }
+
+            ?>
+
             <hr>
 
             <?php 
