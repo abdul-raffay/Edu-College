771422d6e70dd5f9a4238b3db01108ec4894d189
Abdul Raffay <abdul-raffay@nytrotech.com>
2020-06-22 17:18:43 +0500

    Login, Register Done of a Student, Only Admin can access Dashboard, AdminBar Hide Done

--- themes/Edu/functions.php
+++ themes/Edu/functions.php
@@ -13,6 +13,25 @@
 
 
 
+    // Admin Bar Logic
+    function Hide_Admin_Bar(){
+        if(current_user_can('administrator')){
+            show_admin_bar(true);
+        }
+        else{
+            show_admin_bar(false);
+        }
+    }
+    add_action('after_setup_theme', 'Hide_Admin_Bar');
+
+    // Only Admin Can go to Dashboard
+    function only_admin_dashboard() { 
+        if (is_admin() && !current_user_can( 'administrator' )) { 
+            wp_redirect(home_url()); 
+            exit; 
+        } 
+    } 
+    add_action('init', 'only_admin_dashboard'); 
     
 
 
--- themes/Edu/page.php
+++ themes/Edu/page.php
@@ -6,6 +6,8 @@
             the_post();
 
             the_title();
+
+            the_content();
         }
 
     ?>
